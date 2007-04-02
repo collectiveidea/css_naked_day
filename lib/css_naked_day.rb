e290abb9775b049eee49780a6b8b3f693bee0ba5
Daniel Morrison <daniel@collectiveidea.com>
2007-04-02 06:15:43 +0000

    Added a CSS Naked Day plugin

---
+++ lib/css_naked_day.rb
@@ -0,0 +1,21 @@
+ActionView::Helpers::AssetTagHelper.module_eval do
+    
+  def stylesheet_link_tag_with_css_naked_day(*sources)
+    if Date.today.is_css_naked_day?
+      "<!-- No CSS today, It's CSS Naked Day!  See http://naked.dustindiaz.com/ for more info. -->"
+    else
+      stylesheet_link_tag_without_css_naked_day(*sources)
+    end
+  end
+
+  alias_method_chain :stylesheet_link_tag, :css_naked_day
+end
+
+#
+# Add a method to Date for checking CSS Naked Day
+#
+class Date
+  def is_css_naked_day?
+    month == 4 && day == 5
+  end
+end
---
+++ tasks/css_naked_day_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :css_naked_day do
+#   # Task goes here
+# end
